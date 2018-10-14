Focal length calculation corrected.

Homo-Framework-PNP.py
@@ -132,7 +132,14 @@ def extract_circular_points(trajectory_lines, P, method):
 
     return intersections, mean_intersection
 
-def compute_homography_and_warp(image, vp1, vp2, trajectories, corners, clip=True, clip_factor=3, method="posture"):
+def compute_homography_and_warp(image,
+                                vp1, vp2,
+                                trajectories,
+                                corners,
+                                clip=True, clip_factor=3, method="posture"):
+
+    # TODO: Method parameter is useless
+
     """Compute homography from vanishing points and warp the image.
 
     It is assumed that vp1 and vp2 correspond to horizontal and vertical
@@ -349,7 +356,7 @@ def rectify_groundPlane(image_path, segmented_img_path, detection_data_file, fra
     # Parameter : The sample taken every few frames can become a performance concern
     # TODO: Turn tracked pedestrian id's into parameters
     pedestrian_posture_paths, pedestrian_postures = horizon_detector.HorizonDetectorLib.parse_pedestrian_detection(np.copy(image), detection_data_file, 65, use_bounding_boxes=False, returnPosture= True)
-    pedestrian_posture_paths_single, _ = horizon_detector.HorizonDetectorLib.parse_pedestrian_detection(np.copy(image), detection_data_file, 5, use_bounding_boxes=False, tracker_id=[56,17,10]) # Parameter
+    pedestrian_posture_paths_single, _ = horizon_detector.HorizonDetectorLib.parse_pedestrian_detection(np.copy(image), detection_data_file, 5, use_bounding_boxes=False, tracker_id=[56,17,36,44]) # Parameter
 
     #If we assume the people doesn't change their velocities much
     #and calculate a homography between a path with multiple detections
@@ -368,13 +375,13 @@ def rectify_groundPlane(image_path, segmented_img_path, detection_data_file, fra
     # - Feet Trajectory + Hough Lines from the navigable area, RANSAC based
     # - Hough Lines from navigable area only
     vp_determination_methods = {
-        'posture': [pedestrian_posture_paths, False],
-        'single': [pedestrian_posture_paths_single, False],
-        'bb': [pedestrian_bb_paths, False],
-        'hough': [image_lines, True],
-        # 'trajectory': [trajectory_lines, True], DONT USE
-        'trajectory_hough': [[np.concatenate((trajectory_lines[j], image_lines[j]), axis=0)
-                              for j in range(3)] , True],
+        # 'posture': [pedestrian_posture_paths, False],
+        # 'single': [pedestrian_posture_paths_single, False],
+        # 'bb': [pedestrian_bb_paths, False],
+        # 'hough': [image_lines, True],
+        # # 'trajectory': [trajectory_lines, True], DONT USE
+        # 'trajectory_hough': [[np.concatenate((trajectory_lines[j], image_lines[j]), axis=0)
+        #                       for j in range(3)] , True],
         'postures_hough': [[np.concatenate((pedestrian_posture_paths[j], image_lines[j]), axis=0)
                               for j in range(3)], True]
     }
@@ -416,7 +423,7 @@ def rectify_groundPlane(image_path, segmented_img_path, detection_data_file, fra
                                                                                                    zenith=zenith_vp)
             zenith_vp = zenith_vp / zenith_vp[2]
             fov = math.degrees(2 * math.atan2(height , (2 * focal_length)))
-
+            focal_length = height * (1 / np.tan(np.deg2rad(fov / 2))) / 2
             print("Method: {}, left: {}, right: {}".format(k,leftVP[:2],rightVP[:2]))
             print("Zenith: {}, focal: {} with fov: {}".format(zenith_vp[:2], focal_length, fov))
 
@@ -424,65 +431,63 @@ def rectify_groundPlane(image_path, segmented_img_path, detection_data_file, fra
             plot_axis.plot((zenith_vp[0]), (zenith_vp[1]), 'go')
             plot_axis.plot((center[0]), (center[1]), 'yo')
 
+            if True:
+                # Complete stratified approach that rectifies and translates the navigable area
+                warped_result, model_points, H = compute_homography_and_warp(segmented_img,
+                                                                             list(leftVP),
+                                                                             list(rightVP),
+                                                                             trajectory_lines,
+                                                                             image_points,
+                                                                             clip=True,
+                                                                             clip_factor=3,
+                                                                             method=k)
+                plot_axis = rectified_axis[i//col, i % col]
+                plot_axis.set_title(str(k))
+                plot_axis.imshow(warped_result)
 
-            # Complete stratified approach that rectifies and translates the navigable area
-            warped_result, model_points, H = compute_homography_and_warp(segmented_img,
-                                                                         list(leftVP),
-                                                                         list(rightVP),
-                                                                         trajectory_lines,
-                                                                         image_points,
-                                                                         clip=True,
-                                                                         clip_factor=3,
-                                                                         method=k)
-            plot_axis = rectified_axis[i//col, i % col]
-            plot_axis.set_title(str(k))
-            plot_axis.imshow(warped_result)
-
-            for i in range(len(model_points)):
-                plot_axis.plot([model_points[i][0], model_points[(i + 1) % 4][0]],
-                         [model_points[i][1], model_points[(i + 1) % 4][1]], 'b')
+                for i in range(len(model_points)):
+                    plot_axis.plot([model_points[i][0], model_points[(i + 1) % 4][0]],
+                             [model_points[i][1], model_points[(i + 1) % 4][1]], 'b')
 
 
-            plt.imsave("warped_result_" + k +".png", warped_result)
+                plt.imsave("warped_result_" + k +".png", warped_result)
 
-        if False:
+                #------------ABONDEN DON'T USE--------------
+                # My method using imaginary plane which utilizes 4 point approach
+                # allignedImg, leftVP, rightVP, alligned_corners = allignHorizon(segmented_img, horizon)
+                # warped_result, model_points, H = applyParametretizedHomography(allignedImg, list(leftVP), list(rightVP), alligned_corners, k)
+                # ------------------------------------------
 
-            #------------ABONDEN DON'T USE--------------
-            # My method using imaginary plane which utilizes 4 point approach
-            # allignedImg, leftVP, rightVP, alligned_corners = allignHorizon(segmented_img, horizon)
-            # warped_result, model_points, H = applyParametretizedHomography(allignedImg, list(leftVP), list(rightVP), alligned_corners, k)
-            # ------------------------------------------
+                #Intrinsic matrix for camera, same for both model (rectification camera) and scene camera
+                K = np.array([[focal_length,0,warped_result.shape[1]/2],
+                             [0,focal_length,warped_result.shape[0]/2],
+                             [0,0,1]])
 
-            #Intrinsic matrix for camera, same for both model (rectification camera) and scene camera
-            K = np.array([[focal_length,0,warped_result.shape[1]/2],
-                         [0,focal_length,warped_result.shape[0]/2],
-                         [0,0,1]])
+                #Output the internal and external parameters through a text file
+                # Using the p3p methods, map the model points to image points
+                #TODO: Has concepts from:
+                #   Ezio Malis, Manuel Vargas, and others. Deeper understanding of the homography decomposition for vision-based control. 2007.
 
-            #Output the internal and external parameters through a text file
-            # Using the p3p methods, map the model points to image points
-            #TODO: Has concepts from:
-            #   Ezio Malis, Manuel Vargas, and others. Deeper understanding of the homography decomposition for vision-based control. 2007.
+                corners_homo = [[*corner, 1] for corner in image_points]
 
-            corners_homo = [[*corner, 1] for corner in image_points]
+                left_border = np.cross(corners_homo[0], corners_homo[3])
+                right_border = np.cross(corners_homo[1], corners_homo[2])
+                right_border = right_border / right_border[2]
 
-            left_border = np.cross(corners_homo[0], corners_homo[3])
-            right_border = np.cross(corners_homo[1], corners_homo[2])
-            right_border = right_border / right_border[2]
+                horizon_line = np.cross(horizon[0], horizon[1])
 
-            horizon_line = np.cross(horizon[0], horizon[1])
+                corners_homo[0] = np.cross(left_border, horizon_line)
+                image_points[3] = (corners_homo[0] / corners_homo[0][2])[:2]
+                corners_homo[1] = np.cross(right_border, horizon_line)
+                image_points[2] = (corners_homo[1] / corners_homo[1][2])[:2]
 
-            corners_homo[0] = np.cross(left_border, horizon_line)
-            image_points[3] = (corners_homo[0] / corners_homo[0][2])[:2]
-            corners_homo[1] = np.cross(right_border, horizon_line)
-            image_points[2] = (corners_homo[1] / corners_homo[1][2])[:2]
+                image_points = [[pnt[0], max(0,pnt[1])] for pnt in image_points]
 
-            image_points = [[pnt[0], max(0,pnt[1])] for pnt in image_points]
-
-            cameraCalibration.CameraCalibration.extractCameraParameters(image,
-                                                                        warped_result,
-                                                                        model_points,
-                                                                        image_points,
-                                                                        K, H)
+                cameraCalibration.CameraCalibration.extractCameraParameters(image,
+                                                                            warped_result,
+                                                                            model_points,
+                                                                            image_points,
+                                                                            K, H)
 
     plt.show(horizon_fig)
     plt.show(rectified_fig)

horizon_detection/horizon_detector.py
@@ -27,8 +27,8 @@ class HorizonDetectorLib:
         grayscale_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         edges = feature.canny(grayscale_img, 3)
         # Parameter: Parameter
-        line_segments = transform.probabilistic_hough_line(edges, line_length=100,
-                                                   line_gap=30)
+        line_segments = transform.probabilistic_hough_line(edges,   line_length=100,
+                                                                    line_gap=30)
 
         # Edge detection (canny for now)
         # grayscale_img = cv2.Canny(grayscale_img, threshold1=75, threshold2=200, apertureSize=3)
@@ -290,7 +290,7 @@ class HorizonDetectorLib:
             plot_axis.plot(vp1[0], vp1[1], 'bo')
 
             # Before determining the second VP, remove inliers as they already contributed to first VP
-            path_lines_reduced = HorizonDetectorLib.remove_inliers(vp1, path_lines, 30)
+            path_lines_reduced = HorizonDetectorLib.remove_inliers(vp1, path_lines, 60)
 
             # Find second vanishing point
             model2 = HorizonDetectorLib.ransac_vanishing_point(path_lines_reduced)
@@ -299,7 +299,7 @@ class HorizonDetectorLib:
 
             # Test if we can find the zenith vanishing point
             # Before determining the second VP, remove inliers as they already contributed to first VP
-            path_lines_reduced_again = HorizonDetectorLib.remove_inliers(vp2, path_lines_reduced, 60)
+            path_lines_reduced_again = HorizonDetectorLib.remove_inliers(vp2, path_lines_reduced, 30)
 
             # Find second vanishing point
             model3 = HorizonDetectorLib.ransac_vanishing_point(path_lines_reduced_again)
@@ -401,51 +401,46 @@ class HorizonDetectorLib:
                     # logging.info("Current best model has {} votes at iteration {}".format(
                     #     current_votes.sum(), ransac_iter))
 
-        old = False
-        if not old:
-            # The image center  should be the orthocenter of the triangle
-            left_vp = np.array(horizon[0])
-            right_vp = np.array(horizon[1])
 
-            v1_z = np.cross(best_model, left_vp)
-            v1_z = v1_z / v1_z[2]
-            v2_z = np.cross(best_model, right_vp)
-            v2_z = v2_z / v2_z[2]
+        # The image center  should be the orthocenter of the triangle
+        left_vp = np.array(horizon[0])
+        right_vp = np.array(horizon[1])
 
-            v1_v2_len = np.linalg.norm(left_vp[:2] - right_vp[:2])
-            v1_zen_len = np.linalg.norm(left_vp[:2] - best_model[:2])
-            v2_zen_len = np.linalg.norm(right_vp[:2] - best_model[:2])
+        left_vp = np.array([0,6,1])
+        right_vp = np.array([4,6,1])
+        best_model = np.array([1,3,1])
 
-            v1_angle = np.tan(np.arccos(np.dot(horizon_homogenous, v1_z) / (v1_v2_len * v1_zen_len)))
-            v2_angle = np.tan(np.arccos(np.dot(horizon_homogenous, v2_z) / (v1_v2_len * v2_zen_len)))
-            zenith_angle = np.tan(np.arccos(np.dot(v1_z, v2_z) / (v1_zen_len * v2_zen_len)))
+        horizon_homogenous = np.cross(left_vp, right_vp)
+        horizon_homogenous = horizon_homogenous / horizon_homogenous[2]
 
-            image_center = [0,0,1]
+        v1_v2_len = np.linalg.norm(left_vp - right_vp)
+        v1_zen_len = np.linalg.norm(left_vp- best_model)
+        v2_zen_len = np.linalg.norm(right_vp - best_model)
 
-            image_center[0] = (left_vp[0] * v1_angle +
-                               right_vp[0] * v2_angle +
-                               best_model[0] * zenith_angle) / (v1_angle + v2_angle + zenith_angle)
+        v1_angle = np.tan(np.arccos(np.dot(right_vp - left_vp, best_model - left_vp) / (v1_zen_len * v1_v2_len)))
+        v2_angle = np.tan(np.arccos(np.dot(left_vp - right_vp, best_model - right_vp) / (v2_zen_len * v1_v2_len)))
+        zenith_angle = np.tan(np.arccos(np.dot(right_vp - best_model, left_vp- best_model) / (v1_zen_len * v2_zen_len)))
 
-            image_center[1] = (left_vp[1] * v1_angle +
-                               right_vp[1] * v2_angle +
-                               best_model[1] * zenith_angle) / (v1_angle + v2_angle + zenith_angle)
+        image_center = [0,0,1]
+
+        image_center[0] = (left_vp[0] * v1_angle +
+                           right_vp[0] * v2_angle +
+                           best_model[0] * zenith_angle) / (v1_angle + v2_angle + zenith_angle)
+
+        image_center[1] = (left_vp[1] * v1_angle +
+                           right_vp[1] * v2_angle +
+                           best_model[1] * zenith_angle) / (v1_angle + v2_angle + zenith_angle)
 
 
-            # center_vp_dist = (np.linalg.norm(best_model[:2] - image_center[:2]))
-            center_hor_dist = np.abs(np.dot(np.array(image_center), horizon_homogenous)) / np.linalg.norm(horizon_homogenous[:2])
-            # focal_length = np.sqrt(center_vp_dist * center_hor_dist)
+        # center_vp_dist = (np.linalg.norm(best_model[:2] - image_center[:2]))
+        center_hor_dist = np.abs(np.dot(np.array(image_center), horizon_homogenous)) / np.linalg.norm(horizon_homogenous[:2])
+        # focal_length = np.sqrt(center_vp_dist * center_hor_dist)
 
-            center_v1 = np.linalg.norm(image_center - left_vp)
-            center_v2 = np.linalg.norm(image_center - right_vp)
+        center_v1 = np.linalg.norm(image_center - left_vp)
+        center_v2 = np.linalg.norm(image_center - right_vp)
 
-            focal_length = np.sqrt(np.abs((np.sqrt(center_v1 ** 2 - center_hor_dist ** 2))*(np.sqrt(center_v2 ** 2 - center_hor_dist ** 2))
-                                   - (center_hor_dist ** 2)))
-
-        # Old way of calculating the focal length
-        if old:
-            center_vp_dist = (np.linalg.norm(best_model[:2] - image_center[:2]))
-            center_hor_dist = np.dot(np.array(image_center), horizon_homogenous)
-            focal_length = np.sqrt(center_vp_dist * center_hor_dist)
+        focal_length = np.sqrt(np.abs((np.sqrt(center_v1 ** 2 - center_hor_dist ** 2))*(np.sqrt(center_v2 ** 2 - center_hor_dist ** 2))
+                               - (center_hor_dist ** 2)))
 
         return best_model, focal_length, image_center
 